28e27921be41b28b170d3beb553872c209b3b344
hojat72elect <hojat72elect@gmail.com>
2026-08-20 02:08:15 -0400

    "Example_8" : Simple 3D world.

---
+++ src/example_8/Example_8.py
@@ -0,0 +1,15 @@
+from ursina import Ursina, Sky, Entity
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+app = Ursina()
+Sky()
+ground = Entity(model="plane", texture="ground", collider="mesh", scale=(100, 1, 100))
+player = FirstPersonController(position=(0, 2, -5))
+
+
+def input(key):
+    if key == 'escape':
+        app.quit()
+
+
+app.run()
