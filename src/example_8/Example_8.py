baaeadd9524fe6691c46b673275ba237c7756854
hojat72elect <hojat72elect@gmail.com>
2026-08-20 02:24:28 -0400

    "Example_8" : Added walls to the world.

--- src/example_8/Example_8.py
+++ src/example_8/Example_8.py
@@ -1,10 +1,16 @@
-from ursina import Ursina, Sky, Entity
+from ursina import Ursina, Sky, Entity, color, duplicate
 from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 Sky()
 ground = Entity(model="plane", texture="grass", collider="mesh", scale=(100, 1, 100))
 player = FirstPersonController(position=(0, 2, -5))
+
+wall1 = Entity(model="cube", texture="wall", collider="box", scale=(100, 10, 5), position=(0, 5, 50), color=color.gray)
+wall2 = duplicate(wall1, z=-50)
+wall3 = duplicate(wall1, rotation_y=90, x=-50, z=0)
+wall4 = duplicate(wall3, x=50)
+wall5 = duplicate(wall1, position=(0, 2, 0), scale=(20, 5, 0.5), color=color.dark_gray)
 
 
 def input(key):
