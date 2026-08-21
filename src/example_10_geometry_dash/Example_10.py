2fee78c7095f024b9291f3d83fa1bcda916920f7
hojat72elect <hojat72elect@gmail.com>
2026-08-21 16:09:05 -0400

    "Example_10" : Added the ground.

--- src/example_10_geometry_dash/Example_10.py
+++ src/example_10_geometry_dash/Example_10.py
@@ -1,9 +1,10 @@
-from ursina import Ursina, application, Entity, camera
+from ursina import Ursina, application, Entity, camera, color, time
 
 app = Ursina()
 
 background = Entity(model="quad", texture="BG2", scale=55, z=10, y=15)
 player = Entity(model="quad", collider="box", texture="square")
+ground = Entity(model="cube", color=color.yellow, y=-1, origin_y=.5, scale=(200, 15, 1), collider="box", texture="white_cube")
 
 camera.orthographic = True
 camera.fov = 18
@@ -12,6 +13,12 @@ camera.fov = 18
 def input(key):
     if key == 'escape':
         application.quit()
+
+
+def update():
+    if not player.intersects().hit:
+        player.y -= time.dt
+    player.y = max(-0.5, player.y)
 
 
 app.run()
