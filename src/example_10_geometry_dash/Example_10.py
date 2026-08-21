f0b447fb4347933082506c9152c9b02e7b89ed88
hojat72elect <hojat72elect@gmail.com>
2026-08-21 16:03:14 -0400

    "Example_10" : Added a player.

--- src/example_10_geometry_dash/Example_10.py
+++ src/example_10_geometry_dash/Example_10.py
@@ -3,9 +3,11 @@ from ursina import Ursina, application, Entity, camera
 app = Ursina()
 
 background = Entity(model="quad", texture="BG2", scale=55, z=10, y=15)
+player = Entity(model="quad", collider="box", texture="square")
 
 camera.orthographic = True
-camera.fov=18
+camera.fov = 18
+
 
 def input(key):
     if key == 'escape':
