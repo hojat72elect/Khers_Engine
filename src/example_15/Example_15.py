4d7b36df9af2714fecdb4330e8d42ab1336c51f9
hojat72elect <hojat72elect@gmail.com>
2026-08-22 17:32:56 -0400

    "Example_15" : Now the plane moves better.

--- src/example_15/Example_15.py
+++ src/example_15/Example_15.py
@@ -15,8 +15,16 @@ def input(key):
     if key == 'escape':
         application.quit()
 
+
 def update():
     me.y += held_keys["w"] * 6 * time.dt
     me.y -= held_keys["s"] * 6 * time.dt
+    a = held_keys["w"] * -20
+    b = held_keys["s"] * 20
+    if a != 0:
+        me.rotation_z = a
+    else:
+        me.rotation_z = b
+
 
 app.run()
