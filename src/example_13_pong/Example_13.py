69ad2a41771fc575dd73722ab13b7002d7e86451
hojat72elect <hojat72elect@gmail.com>
2026-08-22 15:54:28 -0400

    "Example_13" : Alpha version of the game completed.

--- src/example_13_pong/Example_13.py
+++ src/example_13_pong/Example_13.py
@@ -9,7 +9,7 @@ ball = Entity(model="sphere", color=color.cyan, z=-1, scale=0.1, collider="box")
 player1 = Entity(model="cube", color=color.cyan, scale=(0.6, 0.1, 1), position=(0, -1.4, -1), collider="box")
 player2 = duplicate(player1, y=1.4)
 
-speed_x = speed_y = 0.1
+speed_x = speed_y = 0.2
 
 
 def update():
@@ -24,6 +24,10 @@ def update():
     player1.x -= held_keys["a"] * time.dt
     player2.x += held_keys["right arrow"] * time.dt
     player2.x -= held_keys["left arrow"] * time.dt
+    if ball.intersects().hit:
+        speed_y = -speed_y
+        speed_x *= 1.1
+        speed_y *= 1.1
 
 
 camera.orthographic = True
