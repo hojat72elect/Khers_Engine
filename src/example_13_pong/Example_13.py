0e052b5b2ab72e69f7d17394bb4e1d118552cdfb
hojat72elect <hojat72elect@gmail.com>
2026-08-22 15:21:12 -0400

    "Example_13" : Added Movement to our players.

--- src/example_13_pong/Example_13.py
+++ src/example_13_pong/Example_13.py
@@ -1,4 +1,4 @@
-from ursina import Ursina, window, color, Entity, camera, application, duplicate
+from ursina import Ursina, window, color, Entity, camera, application, duplicate, time, held_keys
 
 app = Ursina()
 
@@ -8,6 +8,23 @@ table = Entity(model="cube", color=color.black, scale=(2, 1, 3), rotation=(90, 0
 ball = Entity(model="sphere", color=color.cyan, z=-1, scale=0.1, collider="box")
 player1 = Entity(model="cube", color=color.cyan, scale=(0.6, 0.1, 1), position=(0, -1.4, -1), collider="box")
 player2 = duplicate(player1, y=1.4)
+
+speed_x = speed_y = 0.1
+
+
+def update():
+    global speed_x, speed_y
+    ball.x += speed_x * time.dt
+    ball.y += speed_y * time.dt
+    if abs(ball.x) > 0.9:
+        speed_x = -speed_x
+    if abs(ball.y) > 1.4:
+        ball.x = ball.y = 0
+    player1.x += held_keys["d"] * time.dt
+    player1.x -= held_keys["a"] * time.dt
+    player2.x += held_keys["right arrow"] * time.dt
+    player2.x -= held_keys["left arrow"] * time.dt
+
 
 camera.orthographic = True
 camera.fov = 4
