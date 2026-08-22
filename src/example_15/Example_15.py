b5179e79255de875386ebc8d2a08863864039fe0
hojat72elect <hojat72elect@gmail.com>
2026-08-22 17:29:50 -0400

    "Example_15" : Added a background.

--- src/example_15/Example_15.py
+++ src/example_15/Example_15.py
@@ -1,4 +1,4 @@
-from ursina import Ursina, Animation, Sky, camera, application
+from ursina import Ursina, Animation, Sky, camera, application, Entity, held_keys, time
 
 app = Ursina()
 
@@ -8,10 +8,15 @@ Sky()
 camera.orthographic = True
 camera.fov = 20
 
+Entity(model="quad", texture="BG", scale=36, z=1)
+
 
 def input(key):
     if key == 'escape':
         application.quit()
 
+def update():
+    me.y += held_keys["w"] * 6 * time.dt
+    me.y -= held_keys["s"] * 6 * time.dt
 
 app.run()
