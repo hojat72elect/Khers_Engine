574490b6ab905a300d137066d2fae47648796edf
hojat72elect <hojat72elect@gmail.com>
2026-08-21 21:10:53 -0400

    "Example_11" : Added the background.

--- src/example_11_deno_run/Example_11.py
+++ src/example_11_deno_run/Example_11.py
@@ -1,9 +1,10 @@
-from ursina import Ursina, window, color, Animation, camera, application
+from ursina import Ursina, window, color, Animation, camera, application, Entity
 
 app = Ursina()
 window.color = color.white
 
 deno = Animation("dino", collider="box", x=-5)
+ground1 = Entity(model="quad", texture="ground", scale=(50, 0.5, 1), z=1)
 
 camera.orthographic = True
 camera.fov = 10
