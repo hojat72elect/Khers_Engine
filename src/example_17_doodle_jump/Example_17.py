b95b24ecdd4b32412ff3e5438c6785ad40d23669
hojat72elect <hojat72elect@gmail.com>
2026-08-22 23:14:25 -0400

    "Example_17" : Bird goes up by touching the platforms.

--- src/example_17_doodle_jump/Example_17.py
+++ src/example_17_doodle_jump/Example_17.py
@@ -1,4 +1,6 @@
-from ursina import Ursina, Sky, Animation, color, camera, SmoothFollow, application, held_keys, time
+from random import randint
+
+from ursina import Ursina, Sky, Animation, color, camera, SmoothFollow, application, held_keys, time, Entity, duplicate, curve, destroy
 
 app = Ursina()
 Sky()
@@ -6,7 +8,11 @@ Sky()
 bird = Animation("bird", collider="box", color=color.orange, y=15)
 camera.add_script(SmoothFollow(target=bird, offset=[0, 0, -40], speed=6))
 
-# platform =
+platform = Entity(model="cube", color=color.green, texture="white_cube", collider="box", scale=(3, 0.5))
+plates = []
+for i in range(5):
+    p = duplicate(platform, y=platform.y + 5)
+    plates.append(p)
 
 
 def input(key):
@@ -17,6 +23,13 @@ def input(key):
 def update():
     bird.x -= held_keys["a"] * 12 * time.dt
     bird.x += held_keys["d"] * 12 * time.dt
+    bird.y -= 7 * time.dt
+    if bird.intersects().hit:
+        bird.animate_y(bird.y + 7, duration=0.3, curve=curve.in_circ)
+        plates.append(duplicate(platform, y=plates[-1].y + 5, x=randint(-5, 5)))
+        obj = plates[0]
+        plates.pop(0)
+        destroy(obj)
 
 
 app.run()
