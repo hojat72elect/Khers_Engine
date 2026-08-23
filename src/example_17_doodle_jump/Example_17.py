ba69e75f6a9b1c64091b397bb238652997669cc1
hojat72elect <hojat72elect@gmail.com>
2026-08-22 23:03:26 -0400

    "Example_17" : Bird can go right and left; and camera will smoothly follow it.

--- src/example_17_doodle_jump/Example_17.py
+++ src/example_17_doodle_jump/Example_17.py
@@ -1,4 +1,4 @@
-from ursina import Ursina, Sky, Animation, color, camera, SmoothFollow, application
+from ursina import Ursina, Sky, Animation, color, camera, SmoothFollow, application, held_keys, time
 
 app = Ursina()
 Sky()
@@ -6,9 +6,17 @@ Sky()
 bird = Animation("bird", collider="box", color=color.orange, y=15)
 camera.add_script(SmoothFollow(target=bird, offset=[0, 0, -40], speed=6))
 
+# platform =
+
 
 def input(key):
     if key == 'escape':
         application.quit()
+
+
+def update():
+    bird.x -= held_keys["a"] * 12 * time.dt
+    bird.x += held_keys["d"] * 12 * time.dt
+
 
 app.run()
