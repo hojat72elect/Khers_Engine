76c09918a450022d0b6649a86a6c46a1d0bc46ae
hojat72elect <hojat72elect@gmail.com>
2026-08-22 22:51:41 -0400

    "Example_16" : Added an army of 100 robots.

--- src/example_16_robot_army/Example_16.py
+++ src/example_16_robot_army/Example_16.py
@@ -5,7 +5,10 @@ app = Ursina()
 player = FirstPersonController()
 ground = Entity(model="plane", texture="grass", collider="mesh", scale=(100, 1, 100))
 
-robot = FrameAnimation3d("robot", position=(2, 0, 2), fps=18, scale=0.015, color=color.white66)
+for i in range(10):
+    for j in range(10):
+        robot = FrameAnimation3d("robot", position=(2 * i, 0, 2 * j), fps=18, scale=0.015, color=color.black66)
+
 Sky()
 
 
