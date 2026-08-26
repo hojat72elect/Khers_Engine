7bafd1c156124822418da92c99ddea5996af8344
hojat72elect <hojat72elect@gmail.com>
2026-08-26 15:02:38 -0400

    Example 10 : Moved assets to their folder.
    #1

--- src/example_10_geometry_dash/Example_10.py
+++ src/example_10_geometry_dash/Example_10.py
@@ -2,8 +2,8 @@ from ursina import Ursina, application, Entity, camera, color, time, curve, dupl
 
 app = Ursina()
 
-background = Entity(model="quad", texture="BG2", scale=55, z=10, y=15)
-player = Entity(model="quad", collider="box", texture="square")
+background = Entity(model="quad", texture="assets/BG2", scale=55, z=10, y=15)
+player = Entity(model="quad", collider="box", texture="assets/square")
 ground = Entity(model="cube", color=color.yellow, y=-1, origin_y=.5, scale=(200, 15, 1), collider="box", texture="white_cube")
 
 camera.orthographic = True
@@ -33,7 +33,7 @@ def input(key):
     if key == "space":
         if player.intersects().hit:
             player.animate_y(player.y + 3, duration=0.3, curve=curve.out_sine)
-            player.animate_rotation_z(player.rotation_z + 180, duration=0.5, curve=curve.linear)
+            player.animate_rotation_z(player.rotation_z + 180, duration=0.5, curve=curve.out_cubic)
             dust = Entity(model=Circle(), scale=-3, color=color.smoke, position=player.position)
             dust.animate_scale(2, duration=0.3, curve=curve.linear)
             dust.fade_out(duration=0.2)
