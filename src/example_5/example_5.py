ffae620ef7d59f398b766143ace7f37ea50dbd23
hojat72elect <hojat72elect@gmail.com>
2026-08-26 14:33:35 -0400

    Example 5 : Cleaned up.
    #1

--- src/example_5/example_5.py
+++ src/example_5/example_5.py
@@ -1,17 +1,21 @@
-from ursina import Ursina, Entity, hsv, curve, EditorCamera, DirectionalLight
+from ursina import Ursina, Entity, hsv, curve, EditorCamera, DirectionalLight, application
 
 app = Ursina()
-
-DirectionalLight(y=2, z=-3, shadows=True)
-
-cube = Entity(model="cube", color=hsv(120, 1, 1), scale=2, collider="box", texture="white_cube")
+DirectionalLight(y=2, z=-3)
+cube = Entity(model="cube", color=hsv(490, 4, 1), scale=2, collider="box", texture="white_cube")
 
 
 def spin():
-    cube.animate("rotation_y", cube.rotation_y + 360, duration=2, curve=curve.in_out_expo)
+    cube.animate("rotation_y", cube.rotation_y + 360, duration=6, curve=curve.out_cubic)
 
 
 cube.on_click = spin
 EditorCamera()
+
+
+def input(key):
+    if key == 'escape':
+        application.quit()
+
 
 app.run()
