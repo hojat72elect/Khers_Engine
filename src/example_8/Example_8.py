6c5ccbfb2f5ba99be677c8e87d67c5d9adb1106a
hojat72elect <hojat72elect@gmail.com>
2026-08-26 14:57:15 -0400

    Example 8 : Moved assets to their folder.
    #1

--- src/example_8/Example_8.py
+++ src/example_8/Example_8.py
@@ -8,10 +8,10 @@ player = FirstPersonController()
 boxes = []
 for n in range(12):
     for k in range(12):
-        box = Button(color=color.white, model="cube", position=(k, 0, n), texture="grass", parent=scene, origin_y=0.5)
+        box = Button(color=color.white, model="cube", position=(k, 0, n), texture="assets/grass", parent=scene, origin_y=0.5)
         boxes.append(box)
 
-sword = Entity(model="blade", texture="sword", rotation=(30, -40), position=(0.5, -0.6), parent=camera.ui, scale=(0.2, 0.15))
+sword = Entity(model="blade", texture="assets/sword", rotation=(30, -40), position=(0.5, -0.6), parent=camera.ui, scale=(0.2, 0.15))
 
 
 def input(key):
@@ -20,7 +20,7 @@ def input(key):
     for box in boxes:
         if box.hovered:
             if key == "left mouse down":
-                new = Button(color=color.white, model="cube", position=box.position + mouse.normal, texture="grass", parent=scene, origin_y=0.5)
+                new = Button(color=color.white, model="cube", position=box.position + mouse.normal, texture="assets/grass", parent=scene, origin_y=0.5)
                 boxes.append(new)
             if key == "right mouse down":
                 boxes.remove(box)
