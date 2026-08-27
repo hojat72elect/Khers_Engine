e1d7a7d0f34996b3e97b24a003342e2ed1d81889
hojat72elect <hojat72elect@gmail.com>
2026-08-26 22:46:27 -0400

    Example 16 : Moved assets to their folder.
    #1

--- src/example_16_robot_army/Example_16.py
+++ src/example_16_robot_army/Example_16.py
@@ -7,7 +7,7 @@ ground = Entity(model="plane", texture="grass", collider="mesh", scale=(100, 1, 
 
 for i in range(10):
     for j in range(10):
-        robot = FrameAnimation3d("robot", position=(2 * i, 0, 2 * j), fps=18, scale=0.015, color=color.black66)
+        robot = FrameAnimation3d("assets/robot", position=(2 * i, 0, 2 * j), fps=18, scale=0.015, color=color.black66)
 
 Sky()
 
