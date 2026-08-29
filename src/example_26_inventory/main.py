bb298b9c1d11084892472b875da7113bbcb7ab4d
hojat72elect <hojat72elect@gmail.com>
2026-08-29 16:28:56 -0400

    Added "Inventory" game to my examples.
    #3

---
+++ src/example_26_inventory/main.py
@@ -0,0 +1,22 @@
+from random import choice
+from ursina import Ursina, Button, color, Tooltip, Entity, camera, Cursor, mouse
+from example_26_inventory.Inventory import Inventory
+
+if __name__ == '__main__':
+    app = Ursina()
+    inventory = Inventory()
+
+
+    def add_item():
+        inventory.append(choice(("bag", "bow_arrow", "gem", "orb", "sword")))
+
+
+    add_item()
+    add_item()
+    add_item_button = Button(scale=(0.1, 0.1), x=-0.5, color=color.lime.tint(-0.25), text="+", tooltip=Tooltip("Add random item"), on_click=add_item)
+    background = Entity(parent=camera.ui, model="quad", texture="shore", scale_x=camera.aspect_ratio, z=1)
+
+    Cursor(texture="cursor", scale=0.1)
+    mouse.visible = False
+
+    app.run()
