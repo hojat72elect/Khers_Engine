4b11953e7f92fee759202497edc244408c4a1fa4
hojat72elect <hojat72elect@gmail.com>
2026-09-03 19:04:07 -0400

    moved manual tests of vec2.py to unit tests.
    #6

--- src/ursina/vec2.py
+++ src/ursina/vec2.py
@@ -1,16 +1,11 @@
 from panda3d.core import Vec2 as PandaVec2
 
-
-
-
 class Vec2(PandaVec2):
     def __round__(self, decimals=4):
         return Vec2(round(self[0], decimals), round(self[1], decimals))
 
-
     def __repr__(self):
         return super().__repr__().replace('LVector2f', 'Vec2')
-
 
     def __iadd__(self, value):
             for i in range(0, len(value), 2):
@@ -18,14 +13,11 @@ class Vec2(PandaVec2):
                 self.add_y(value[i+1])
             return self
 
-
     def __add__(self, value):
         return Vec2(self[0]+value[0], self[1]+value[1])
 
-
     def __sub__(self, value):
         return Vec2(self[0]-value[0], self[1]-value[1])
-
 
     @property
     def x(self):
@@ -64,18 +56,13 @@ class Vec2(PandaVec2):
     def XY(self):
         return (self.X, self.Y)
 
-
-
     def __mul__(self, value):
         if isinstance(value, (int, float, complex)):
             return Vec2(*(e*value for e in self))
 
         return Vec2(self[0]*value[0], self[1]*value[1])
 
-
     __rmul__ = __mul__
-
-
 
     def __truediv__(self, value):
         if isinstance(value, (int, float, complex)):
@@ -83,10 +70,8 @@ class Vec2(PandaVec2):
 
         return Vec2(self[0]/value[0], self[1]/value[1])
 
-
     def __abs__(self):
         return Vec2(*[abs(e) for e in self])
-
 
 Vec2.zero = Vec2(0,0)
 Vec2.one = Vec2(1,1)
@@ -98,10 +83,3 @@ Vec2.down = Vec2(0,-1)
 Vec2.cardinal_directions = (Vec2.up, Vec2.right, Vec2.down, Vec2.left)
 Vec2.ordinal_directions = (Vec2(1,1), Vec2(1,-1), Vec2(-1,-1), Vec2(-1,1))
 Vec2.compass_directions = Vec2.cardinal_directions + Vec2.ordinal_directions
-
-
-
-if __name__ == '__main__':
-    a = Vec2(1,1)
-    print(a)
-    print(round(a))
---
+++ test/test_vec2.py
@@ -0,0 +1,36 @@
+from ursina import Vec2
+
+class TestVec2:
+
+    def test_general_behavior(self):
+        sut = Vec2(1, 1)
+        assert sut.X == 1
+        assert sut.Y == 1
+        assert sut.XY == (1, 1)
+
+        sut.x = 3
+        sut.y = 3
+        assert sut.X == 3
+        assert sut.Y == 3
+        assert sut.XY == (3, 3)
+
+    def test_rounding_a_vec2(self):
+        aVector = Vec2(3.9, 5.2)
+        sut = round(aVector)
+
+        assert sut.X == 3
+        assert sut.Y == 5
+
+    def test_vec2_constants(self):
+        assert Vec2.zero == Vec2(0,0)
+        assert Vec2.one == Vec2(1, 1)
+        assert Vec2.right == Vec2(1, 0)
+        assert Vec2.left == Vec2(-1, 0)
+        assert Vec2.up == Vec2(0, 1)
+        assert Vec2.down == Vec2(0, -1)
+
+        assert Vec2.cardinal_directions == (Vec2.up, Vec2.right, Vec2.down, Vec2.left)
+        assert Vec2.ordinal_directions == (Vec2(1, 1), Vec2(1, -1), Vec2(-1, -1), Vec2(-1, 1))
+        assert Vec2.compass_directions == Vec2.cardinal_directions + Vec2.ordinal_directions
+
+
