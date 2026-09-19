9663cd20f59d44d8683b522ae2b91721dfc12467
hojat72elect <hojat72elect@gmail.com>
2026-09-18 22:48:46 -0400

    Added sphinx configurations (src path, extensions, and so on).
    #14

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,28 +1,21 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+sys.path.insert(0, os.path.abspath("../../src"))
 
 project = 'Khers Engine'
 copyright = '2026, hojat72elect'
 author = 'hojat72elect'
 release = '0.1.1'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
